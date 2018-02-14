e8f34d1324a9c84fb532afc4c6908f3a3074da0a
Zeus <erick@merglabs.com>
2018-02-14 07:13:32 +0300

    Solved .rvm issues

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
 source $HOME/bin/init/todo_completion
-# source $HOME/.rvm/scripts/rvm
+source $HOME/.rvm/scripts/rvm
 source $HOME/bin/init/aliases.sh
 source $HOME/bin/init/exports.sh
 source $HOME/bin/init/scripts/z.sh
