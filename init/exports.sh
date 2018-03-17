353b002233b410d02bd6b876ec8550b9c408d4f4
Zeus <erick@merglabs.com>
2018-03-18 02:00:11 +0300

    exports

--- init/exports.sh
+++ init/exports.sh
@@ -1,4 +1,5 @@
 export PATH=$HOME/bin/init/scripts:$PATH
 export PATH=$HOME/.config/composer/vendor/bin:$PATH
+export PATH=$HOME/.yarn/bin:$PATH
 export JOURN_PATH=$HOME/Documents/journ/
 export EDITOR=vim
