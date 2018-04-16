557aac57f1e4216e5f28d501072013f130355898
Zeus <erick@merglabs.com>
2018-04-16 10:01:40 +0300

    Editor from vim to subl

--- init/exports.sh
+++ init/exports.sh
@@ -2,4 +2,4 @@ export PATH=$HOME/bin/init/scripts:$PATH
 export PATH=$HOME/.config/composer/vendor/bin:$PATH
 export PATH=$HOME/.yarn/bin:$PATH
 export JOURN_PATH=$HOME/tools/journ/
-export EDITOR=vim
+export EDITOR=subl
